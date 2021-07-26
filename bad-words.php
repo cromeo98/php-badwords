2af04d5f5df257dd5ed68449490b2033c6a895f9
cromeo98 <romeo.christian98@gmail.com>
2021-07-26 15:28:52 +0200

    var dump of replacement

--- bad-words.php
+++ bad-words.php
@@ -1,6 +1,18 @@
+<!-- 
+    1 Creare una variabile con un paragrafo di testo a vostra scelta. V
+    2 Stampare a schermo il paragrafo e la sua lunghezza. V
+    3 Una parola da censurare viene passata dall'utente tramite parametro GET. 
+    4 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
 <?php
-$word = 'ginos'
+
+$my_string = 'I topi non avevano nipoti';
+
+$bad_word = ($_GET['badWord']);
+
+var_dump (str_replace($bad_word, '***', $my_string));
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,7 +25,10 @@ $word = 'ginos'
 <body>
     
     <p>
-        <?php echo $word ?>
+        <?php echo $my_string?>
+    </p>
+    <p>
+        la lunghezza della stringa è: <?php echo strlen($my_string)?>
     </p>
 
 </body>
