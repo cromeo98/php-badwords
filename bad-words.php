257af531362f2c64bd3c21cdb3e34e7b4d420fbf
cromeo98 <romeo.christian98@gmail.com>
2021-07-26 14:58:07 +0200

    first commit

---
+++ bad-words.php
@@ -0,0 +1,20 @@
+<?php
+$word = 'ginos'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bad word exercise</title>
+</head>
+<body>
+    
+    <p>
+        <?php echo $word ?>
+    </p>
+
+</body>
+</html>
